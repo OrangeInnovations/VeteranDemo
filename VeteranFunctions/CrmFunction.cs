c09ab45cf6edf9b56d98daa277867936601e0235
David <forward_chen@hotmail.com>
2020-12-03 13:22:20 -0500

    lines

--- VeteranFunctions/CrmFunction.cs
+++ VeteranFunctions/CrmFunction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -18,9 +19,18 @@ namespace VeteranFunctions
             log.LogInformation($"Log information C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
             log.LogTrace($"Log trace C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-            StreamReader reader = new StreamReader(myBlob);
-            var lines=reader.ReadToEnd();
 
+            List<string> lines = new List<string>();
+
+            using (StreamReader reader = new StreamReader(myBlob))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lines.Add(reader.ReadLine());
+                }
+            }
+
+            log.LogInformation($"Log information - read file {name}, {lines.Count} lines.");
         }
     }
 }
